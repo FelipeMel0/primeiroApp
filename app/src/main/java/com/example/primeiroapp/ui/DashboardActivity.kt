08baa9315ff378f428f6eac1f282386b2b2dbe78
FelipeMel0 <felipedemfernandes@gmail.com>
2021-11-22 08:48:36 -0300

    Corrigindo imagens e calculando idade

--- app/src/main/java/com/example/primeiroapp/ui/DashboardActivity.kt
+++ app/src/main/java/com/example/primeiroapp/ui/DashboardActivity.kt
@@ -6,47 +6,96 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import com.example.primeiroapp.R
+import com.example.primeiroapp.utils.calcularIdade
 import com.example.primeiroapp.utils.convertBase64ToBitMap
 
 class DashboardActivity : AppCompatActivity() {
+
+//    val tvNome = findViewById<TextView>(R.id.tv_nome)
+//    val tvProfissao = findViewById<TextView>(R.id.tv_profissao)
+//    val tvAltura = findViewById<TextView>(R.id.tv_altura)
+//    val tvDataNascimento = findViewById<TextView>(R.id.tv_idade)
+//    val tvImc = findViewById<TextView>(R.id.tv_imc)
+//    val tvNcd = findViewById<TextView>(R.id.tv_ncd)
+//    val tvPeso = findViewById<TextView>(R.id.tv_peso)
+//    val tvIdade = findViewById<TextView>(R.id.tv_idade)
+//    val tvFotoPerfil = findViewById<ImageView>(R.id.iv_dash_foto)
+
+    lateinit var tvNome: TextView
+    lateinit var tvProfissao: TextView
+    lateinit var tvImc: TextView
+    lateinit var tvNcd: TextView
+    lateinit var tvPeso: TextView
+    lateinit var tvIdade: TextView
+    lateinit var tvAltura: TextView
+    lateinit var ivPerfil: ImageView
+    lateinit var tvDataNascimento : TextView
+    lateinit var tvFotoPerfil: ImageView
+//
+//    tvFotoPerfil = findViewById(R.id.iv_dash_foto)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
         supportActionBar!!.hide()
+         tvNome = findViewById(R.id.tv_nome)
+         tvProfissao = findViewById(R.id.tv_profissao)
+         tvAltura = findViewById(R.id.tv_altura)
+         tvDataNascimento = findViewById(R.id.tv_idade)
+         tvImc = findViewById(R.id.tv_imc)
+         tvNcd = findViewById(R.id.tv_ncd)
+         tvPeso = findViewById(R.id.tv_peso)
+         tvIdade = findViewById(R.id.tv_idade)
+         tvFotoPerfil = findViewById(R.id.iv_dash_foto)
+
 
 //        val arquivo = applicationContext.getSharedPreferences("usuario", Context.MODE_PRIVATE)
-        val prefs = getSharedPreferences("usuario", Context.MODE_PRIVATE)
+//        val prefs = getSharedPreferences("usuario", Context.MODE_PRIVATE)
 
-        val tvNome = findViewById<TextView>(R.id.tv_nome)
-        val tvProfissao = findViewById<TextView>(R.id.tv_profissao)
-        val tvAltura = findViewById<TextView>(R.id.tv_altura)
-        val tvDataNascimento = findViewById<TextView>(R.id.tv_idade)
-        val tvImc = findViewById<TextView>(R.id.tv_imc)
-        val tvNcd = findViewById<TextView>(R.id.tv_ncd)
-        val tvPeso = findViewById<TextView>(R.id.tv_peso)
-        val tvFotoPerfil = findViewById<ImageView>(R.id.iv_dash_foto)
+
+
 
 
         //Nome
-        val name = prefs.getString("nome", "")
-        tvNome.setText(name.toString())
+//        val name = prefs.getString("nome", "")
+//        tvNome.setText(name.toString())
+//
+//        //Profissão
+//        val profissao = prefs.getString("profissao", "")
+//        tvProfissao.setText(profissao.toString())
+//
+//        //Altura
+//        val altura = prefs.getFloat("altura", 0.00F)
+//        tvAltura.setText(altura.toString())
+//
+//        //Idade
+////        val idade = prefs.getString("dataNascimento", "")
+////        tvDataNascimento.setText(idade.toString())
+//
+//        tvIdade.text = calcularIdade(prefs.getString("dataNascimento", "")!!).toString()
+//
+//        val bitmap = convertBase64ToBitMap(prefs.getString("fotoPerfil", "")!!)
+//        tvFotoPerfil.setImageBitmap(bitmap)
 
-        //Profissão
-        val profissao = prefs.getString("profissao", "")
-        tvProfissao.setText(profissao.toString())
-
-        //Altura
-        val altura = prefs.getFloat("altura", 0.00F)
-        tvAltura.setText(altura.toString())
-
-        //Idade
-//        val idade = prefs.getString("dataNascimento", "")
-//        tvDataNascimento.setText(idade.toString())
-
-        val bitmap = convertBase64ToBitMap(prefs.getString("fotoPerfil", ""))!!
-        tvFotoPerfil.setImageBitmap(bitmap)
-
+        carregarDashboard()
 
     }
+
+    private fun carregarDashboard() {
+        val arquivo =
+            getSharedPreferences(
+                "usuario", MODE_PRIVATE)
+
+        tvNome.text = arquivo.getString("nome", "")
+        tvProfissao.text = arquivo.getString("profissao", "")
+        tvAltura.text = arquivo.getFloat("altura", 0.0f).toString()
+
+        tvIdade.text = calcularIdade(arquivo.getString("dataNascimento", "")!!).toString()
+
+        val bitmap = convertBase64ToBitMap(arquivo.getString("fotoPerfil", "")!!)
+        tvFotoPerfil.setImageBitmap(bitmap)
+
+    }
+
 }
--- app/src/main/java/com/example/primeiroapp/utils/ConvertBitmapToBase64.kt
+++ app/src/main/java/com/example/primeiroapp/utils/ConvertBitmapToBase64.kt
@@ -7,12 +7,25 @@ import android.util.Base64
 import java.util.*
 
 
-fun encodeImage(bm: Bitmap): String? {
-    val baos = ByteArrayOutputStream()
-    bm.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-    val b = baos.toByteArray()
-//    Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT)
-    return Base64.getEncoder().encodeToString(b)
+//fun encodeImage(bm: Bitmap): String? {
+//    val baos = ByteArrayOutputStream()
+//    bm.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+//    val b = baos.toByteArray()
+////    Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT)
+//    return Base64.getEncoder().encodeToString(b)
+//}
+
+fun encodeImage(bitmap: Bitmap) : String {
+    val bitmapArray = ByteArrayOutputStream()
+
+    bitmap.compress(
+        Bitmap.CompressFormat.JPEG,
+        100,
+        bitmapArray)
+
+    return Base64.encodeToString(
+        bitmapArray.toByteArray(),
+        Base64.DEFAULT)
 }
 
 //fun encodeToBitmap(imagem64: String) : String? {
@@ -20,11 +33,11 @@ fun encodeImage(bm: Bitmap): String? {
 //    val decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
 //}
 
-fun convertBase64ToBitMap(base64: String) : Bitmap{
+fun convertBase64ToBitMap(base64Image: String) : Bitmap{
 
     //Converter Base64 em bytes
 
-    val imageBytes = Base64.decode(base64, Base64.DEFAULT)
+    val imageBytes = Base64.decode(base64Image, Base64.DEFAULT)
 
     return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
 
